dce1e68cdf60ba4c8e0086c1107bea138f3db196
Madson <madson.mms@gmail.com>
2025-12-03 12:48:46 -0300

    First commit

---
+++ player.gd
@@ -0,0 +1,19 @@
+class_name Player
+extends Node2D
+
+var move_speed: float = 100.00
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_up"):
+		pass
+	
