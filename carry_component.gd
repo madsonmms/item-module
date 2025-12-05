208f263b163c51f0eb8b405fad9f2293ad0cf427
Madson <madson.mms@gmail.com>
2025-12-05 17:47:54 -0300

    Drag and drop when button pressed it's ok, now it's time to add movement and physics

--- carry_component.gd
+++ carry_component.gd
@@ -4,11 +4,18 @@ extends Node
 signal item_picked_up(item: BaseItem)
 signal item_dropped(item: BaseItem)
 
+@export var pick_up_area : Area2D
+
 var carried_item: BaseItem = null
 var is_carrying: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	if pick_up_area:
+		pick_up_area.area_entered.connect(_on_item_nearby)
+		pick_up_area.area_exited.connect(_on_item_far)
+	
 	pass # Replace with function body.
 
 
@@ -42,12 +49,21 @@ func drop_item() -> bool:
 	
 	print_debug(carried_item.item_name, " largado!")
 	
+	item_dropped.emit(carried_item)
+	
 	carried_item = null
 	is_carrying = false
 	
-	return true
 	
-	return false
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+	return true
+
+func _on_item_nearby(body: Node) -> void:
+	if body is BaseItem and body.is_pickable:
+		print_debug("Item próximo: ", body.item_name)
 	pass
+	
+func _on_item_far(body: Node) -> void:
+	if body is BaseItem:
+		print_debug("Item afastou: ", body.item_name)
+	pass
+	
