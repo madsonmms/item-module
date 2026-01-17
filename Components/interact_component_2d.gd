5051f53a422483990b4177db2cdedce88a9ec356
Madson <madson.mms@gmail.com>
2026-01-17 20:19:54 -0300

    feature: interact component now handles just interactions

--- Components/interact_component_2d.gd
+++ Components/interact_component_2d.gd
@@ -4,10 +4,11 @@ extends Node2D
 # COMPONENT SIGNALS (for UI, sound, etc.)              
 signal interaction_started(interactable: Interactable)
 signal interaction_completed(interactable: Interactable)
-signal interactable_detected(interactable: Interactable)
-signal interactable_lost()
 
+# Inspector configurations
 @export var carry_point: Marker2D
+@export var detector: Node2D
+@export var detection_enabled: bool = true
 
 # Found interactables
 var available_body: Interactable = null
@@ -22,90 +23,22 @@ var actor: CharacterBody2D
 
 # Detection
 var current_detector : Node = null
-var detection_enabled: bool = true
+var detection: InteractDetector = InteractDetector.new()
 
 
 #-- COMPONENT SETUP :: START --#
 func _ready() -> void:
 	
 	actor = get_parent() as CharacterBody2D
-	_setup_detector()
+	
+	current_detector = detection.setup_detector(detector)
 	
 	pass
 
 
 func _process(_delta: float) -> void:
 	if current_detector is RayCast2D:
-		_update_raycast_detection()
-
-func _setup_detector() -> void:
-	for child in get_children():
-		if child is Area2D or child is RayCast2D:
-			current_detector = child
-			_connect_detector_signals()
-			print_debug("Detector configurado: ", current_detector.name)
-			
-	if not current_detector:
-		push_warning("[InteractionComponent] Nenhum detector configurado ou tipo inválido, insira um detector válido! (Area2D | Raycast2D)")
-
-
-func _connect_detector_signals() -> void:
-	if current_detector is Area2D:
-		current_detector.body_entered.connect(_on_area_body_entered)
-		current_detector.body_exited.connect(_on_area_body_exited)
-	else:
-		return
-
-
-func _update_raycast_detection() -> void:
-	
-	if not detection_enabled or not current_detector is RayCast2D:
-		return
-	
-	var raycast = current_detector as RayCast2D
-	raycast.force_raycast_update()
-	
-	if raycast.is_colliding():
-		var collider = raycast.get_collider()
-		var body = collider as Interactable
-		
-		if body and body.interaction_active:
-			if body == carried_body:
-				if available_body:
-					available_body.notify_detection_end(self)
-					available_body = null
-				return
-			
-			if available_body and available_body != body:
-				available_body.notify_detection_end(self)
-			
-			if body.interaction_active and available_body != body:
-				available_body = body
-				available_body.notify_detection_start(self)
-				interactable_detected.emit(body)
-			
-	elif available_body:
-		available_body.notify_detection_end(self)
-		available_body = null
-		interactable_lost.emit()
-
-
-func _interactable_type_route(interactable: Interactable) -> bool:
-	match available_body.get_interactable_type():
-		available_body.InteractableType.CARRY:
-			_handle_carry(interactable)
-		
-		available_body.InteractableType.INTERACT:
-			return _handle_interact(interactable)
-		
-		available_body.InteractableType.PICKUP:
-			return _handle_pickup(interactable)
-		_:
-			print("Tipo de interação não encontrado")
-			return false
-			
-	return false
-#-- COMPONENT SETUP :: END --#
+		available_body = detection.update_raycast_detector(carried_body)
 
 #-- COMPONENT MAIN :: START --#
 func try_interact() -> bool:
@@ -115,7 +48,8 @@ func try_interact() -> bool:
 		return false
 	
 	if current_detector is Area2D:
-		var nearest = _get_nearest_body()
+		var nearest = detection._get_nearest_body()
+		
 		if nearest:
 			available_body = nearest
 	
@@ -144,11 +78,28 @@ func _handle_carry(body: Interactable) -> bool:
 		return _carry_body(body)
 
 
-func _handle_pickup(body: Interactable) -> bool:
+func _handle_pickup(_body: Interactable) -> bool:
 	print_debug("[InteractComponent] tentando pickup")
 	return true
 	
 #-- COMPONENT MAIN :: END --#
+
+func _interactable_type_route(interactable: Interactable) -> bool:
+	match available_body.get_interactable_type():
+		available_body.InteractableType.CARRY:
+			_handle_carry(interactable)
+		
+		available_body.InteractableType.INTERACT:
+			return _handle_interact(interactable)
+		
+		available_body.InteractableType.PICKUP:
+			return _handle_pickup(interactable)
+		_:
+			print("Tipo de interação não encontrado")
+			return false
+			
+	return false
+#-- COMPONENT SETUP :: END --#
 
 #-- DROP FUNCTIONS :: START --#
 func _carry_body(body: Interactable) -> bool:
@@ -194,52 +145,3 @@ func _calculate_drop_position(carrier: CharacterBody2D) -> Vector2:
 	return carrier.global_position + drop_offset
 	
 #-- DROP FUNCTIONS :: END --#
-
-#-- DETECTION :: START --#
-func _on_area_body_entered(body: Node2D) -> void:
-	var body_detected = body as Interactable
-	
-	available_body = body_detected
-	
-	available_body.notify_detection_start(self)
-	
-	
-func _on_area_body_exited(body: Node2D) -> void:
-	
-	var body_detected = body as Interactable
-	
-	if body_detected and body_detected == available_body:
-		available_body.notify_detection_end(self)
-		available_body = null
-	
-	pass
-		
-
-func _get_nearest_body() -> Node2D:
-	if current_detector is not Area2D:
-		print_debug("Detector não é area2D")
-		return
-	
-	var bodies = current_detector.get_overlapping_bodies()
-	var nearest_body: Node2D = null
-	var min_dist: float = INF
-	
-	for body in bodies:
-		
-		var interactable = body as Interactable
-		
-		if interactable and interactable.interaction_active and interactable != carried_body:
-			var dist = global_position.distance_squared_to(body.global_position)
-			
-			if dist < min_dist:
-				min_dist = dist
-				nearest_body = interactable
-				
-	return nearest_body
-
-	
-func _clear_available_body() -> void:
-	if available_body and available_body != carried_body:
-		available_body.notify_detection_end(self)
-	available_body = null
-#-- DETECTION :: END --#
--- Components/interact_detector.gd
+++ Components/interact_detector.gd
@@ -1,8 +1,119 @@
 class_name InteractDetector
 extends Node
 
+signal interactable_detected(interactable: Interactable)
+signal interactable_lost()
 
-var current_detector: Node2D
+var current_detector: Node
+
+# Found interactables
+var available_body: Interactable = null
+
+func _ready() -> void:
+	
+	pass
 
 func get_detector_type() -> Node2D:
 	return null
+
+func setup_detector(detector) -> Node:
+	if detector is Area2D or detector is RayCast2D:
+		current_detector = detector
+		_connect_detector_signals()
+		print_debug("Detector configurado: ", current_detector.name)
+			
+	if not current_detector:
+		push_warning("[InteractionComponent] Nenhum detector configurado ou tipo inválido, insira um detector válido! (Area2D | Raycast2D)")
+
+	return current_detector
+	
+func update_raycast_detector(_carried_body: Node) -> Node:
+	if not current_detector is RayCast2D:
+		return null
+	
+	var raycast = current_detector as RayCast2D
+	raycast.force_raycast_update()
+	
+	if raycast.is_colliding():
+		var collider = raycast.get_collider()
+		var body = collider as Interactable
+		
+		if body and body.interaction_active:
+			if body == _carried_body:
+				if available_body:
+					available_body.notify_detection_end(self)
+				return null
+			
+			if available_body and available_body != body:
+				available_body.notify_detection_end(self)
+			
+			if body.interaction_active and available_body != body:
+				available_body = body
+				available_body.notify_detection_start(self)
+				interactable_detected.emit(body)
+				return available_body
+			
+	elif available_body:
+		available_body.notify_detection_end(self)
+		interactable_lost.emit()
+		return null
+	
+	return available_body
+
+
+func _connect_detector_signals() -> void:
+	if current_detector is Area2D:
+		current_detector.body_entered.connect(_on_area_body_entered)
+		current_detector.body_exited.connect(_on_area_body_exited)
+	else:
+		return
+
+
+#-- DETECTION :: START --#
+func _on_area_body_entered(body: Node) -> void:
+	var body_detected = body as Interactable
+	
+	available_body = body_detected
+	
+	available_body.notify_detection_start(self)
+	
+	
+func _on_area_body_exited(body: Node) -> void:
+	
+	var body_detected = body as Interactable
+	
+	if body_detected and body_detected == available_body:
+		available_body.notify_detection_end(self)
+		available_body = null
+	
+	pass
+		
+
+func _get_nearest_body() -> Node:
+	if current_detector is not Area2D:
+		print_debug("Detector não é area2D")
+		return
+	
+	var bodies = current_detector.get_overlapping_bodies()
+	var nearest_body: Node2D = null
+	var min_dist: float = INF
+	
+	for body in bodies:
+		
+		var interactable = body as Interactable
+		
+		if interactable and interactable.interaction_active:
+			var dist = current_detector.global_position.distance_squared_to(body.global_position)
+			
+			if dist < min_dist:
+				min_dist = dist
+				nearest_body = interactable
+				
+	return nearest_body
+
+	
+func _clear_available_body() -> void:
+	if available_body:
+		available_body.notify_detection_end(self)
+	available_body = null
+#-- DETECTION :: END --#
